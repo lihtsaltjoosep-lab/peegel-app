afeacf817662ab11b40969512cab7a1f35bbc49f
lihtsaltjoosep-lab <lihtsaltjoosep@gmail.com>
2026-03-20 08:55:52 +0200

    Implement service worker for asset caching
    Add service worker with caching for assets

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = 'peegel-v32-stable';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
